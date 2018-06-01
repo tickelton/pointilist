35e87b3ab866255f4812b96fb0855a0c5b7dd4d1
tickelton <tickelton@gmail.com>
2018-06-01 15:01:18 +0200

    test_graph: added mock HTTP handler

--- pointilist/graph.py
+++ pointilist/graph.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
 # SPDX-License-Identifier:     MIT
 
-import urllib3
+"""
+    Handles the interaction with github contribution graph data.
+
+    :copyright: © 2018, tickelton <tickelton@gmail.com>.
+    :license: MIT, see LICENSE for details.
+"""
+
+import urllib.request
+from urllib.error import URLError, HTTPError
+
 
 class Graph:
     """Object that represents a user's github contribution graph."""
@@ -12,5 +20,17 @@ class Graph:
         self.username = username
 
     def fetch(self):
-        pass
+        """Retrieves contribution data from Github."""
+        url = 'https://github.com/users/' + self.username + '/contributions'
+        try:
+            page = urllib.request.urlopen(url)
+        except HTTPError as err:
+            print("There was a problem fetching data from {}".format(url))
+            print('Error code: ', err.code)
+            return None
+        except URLError as err:
+            print("There was a problem fetching data from {}".format(url))
+            print('Reason: ', err.reason)
+            return None
 
+        return page.readlines()
--- tests/test_graph.py
+++ tests/test_graph.py
@@ -1,43 +1,77 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
 # SPDX-License-Identifier:     MIT
 
+import os
 import unittest
 import urllib.request
 from io import StringIO
 
-def _mock_response_200(req):
-    if req.get_full_url() == "http://example.com":
-        resp = urllib.response.addinfourl(StringIO("mock file"), "mock message", req.get_full_url())
+from pointilist import graph
+
+CONTRIB_HTML = os.path.dirname(os.path.realpath(__file__)) \
+        + '/static/contributions.html'
+
+mock_response_fd = StringIO("foobar")
+
+
+def _mock_response(req):
+    if req.get_full_url() == "https://github.com/users/404/contributions":
+        resp = urllib.response.addinfourl(
+            mock_response_fd,
+            "mock message",
+            req.get_full_url()
+        )
+        resp.code = 404
+        resp.msg = "Not Found"
+        return resp
+    else:
+        resp = urllib.response.addinfourl(
+            mock_response_fd,
+            "mock message",
+            req.get_full_url()
+        )
         resp.code = 200
         resp.msg = "OK"
         return resp
 
-def _mock_response_404(req):
-    if req.get_full_url() == "http://example.com":
-        resp = urllib.response.addinfourl(StringIO("mock file"), "mock message", req.get_full_url())
-        resp.code = 404
-        resp.msg = "OK"
-        return resp
 
-class MockHTTPHandler(urllib.request.HTTPHandler):
+class MockHTTPHandler(urllib.request.HTTPSHandler):
+
+    def https_open(self, req):
+        return self.http_open(req)
 
     def http_open(self, req):
-        return _mock_response_404(req)
+        return _mock_response(req)
 
 
 class TestFetchMethod(unittest.TestCase):
-    
-    def test_fetch_404(self):
+
+    def setUp(self):
         mock_opener = urllib.request.build_opener(MockHTTPHandler)
         urllib.request.install_opener(mock_opener)
-        response=urllib.request.urlopen("http://example.com")
-        print(response.read())
-        print(response.code)
-        print(response.msg)
+
+    def tearDown(self):
+        urllib.request._opener = None
+
+    def test_fetch_valid_graph(self):
+        global mock_response_fd
+
+        with open(CONTRIB_HTML, 'r') as mock_response_fd:
+            g = graph.Graph('200')
+            ret = g.fetch()
+
         self.assertTrue(False)
+
+    def test_fetch_404(self):
+        global mock_response_fd
+
+        mock_response_fd = StringIO("mock 404")
+        g = graph.Graph('404')
+        ret = g.fetch()
+
+        self.assertEqual(ret, None)
+
 
 if __name__ == '__main__':
     unittest.main()
-
